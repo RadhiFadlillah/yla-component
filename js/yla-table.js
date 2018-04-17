ba57e436815ae7843e619ab6340e94dc266b0f32
Radhi Fadlillah <m.radhi.f@gmail.com>
2018-04-17 10:48:40 +0700

    Add selector in table

--- js/yla-table.js
+++ js/yla-table.js
@@ -3,6 +3,11 @@ var YlaTable = function () {
         <div class="yla-table">
             <div class="yla-table__header" 
                  :style="{gridTemplateColumns: headerColumnTemplate}">
+                <div v-if="selectable">
+                    <a class="selector" @click="toggleAllSelection">
+                        <i class="far fa-fw" :class="headerSelectorIcon()"></i>
+                    </a>
+                </div>
                 <div v-for="(col, idx) in finalColumns" 
                      :class="colClass(col)" 
                      :style="{width: colWidth(idx)}"
@@ -19,11 +24,20 @@ var YlaTable = function () {
                     <slot name="empty-message">There are no data available</slot>
                 </div>
                 <template v-else>
-                    <div v-for="col in finalColumns" class="hidden">{{col.label}}{{col.sortable ? 'XXX' : ''}}</div>
+                    <template>
+                        <div v-if="selectable" class="hidden"></div>
+                        <div v-for="col in finalColumns" class="hidden">{{col.label}}{{col.sortable ? 'XXX' : ''}}</div>
+                    </template>
                     <template v-if="summary && contents.length > 0 && !loading">
+                        <div v-if="selectable" class="hidden"></div>
                         <div v-for="col in finalColumns" class="hidden">{{summary[col.name]}}</div>
                     </template>
                     <template v-for="(item, idx) in contents">
+                        <div v-if="selectable" class="selector" :class="{strip: (idx % 2 === 1)}">
+                            <a class="menu" @click="toggleSelection(idx)">
+                                <i class="far fa-fw" :class="selectorIcon(idx)"></i>
+                            </a>
+                        </div>
                         <div v-for="col in finalColumns" :style="{textAlign: col.align}" :class="{strip: (idx % 2 === 1)}">
                             <slot :name="'body-'+col.name" :data="item" :index="idx">{{item[col.name]}}</slot>
                         </div>
@@ -33,6 +47,7 @@ var YlaTable = function () {
             <div class="yla-table__footer" 
                  v-if="summary && contents.length > 0 && !loading" 
                  :style="{gridTemplateColumns: headerColumnTemplate}">
+                <div v-if="selectable"></div>
                 <div v-for="(col, idx) in finalColumns" 
                      :style="{textAlign: col.align, width: colWidth(idx)}">
                     {{summary[col.name]}}
@@ -65,22 +80,21 @@ var YlaTable = function () {
         return body.offsetWidth - body.clientWidth;
     }
 
-    function _getColumnWidth(table, columns, contents) {
+    function _getColumnWidth(table, contents, selectable) {
         // Make sure the table element is exist
         if (!table) return [];
 
-        // If there are no columns, stop
-        if (columns.constructor !== Array) return [];
-
-        // If there are contents, stop
+        // If there are no contents, stop
         if (contents.constructor !== Array || contents.length === 0) return [];
 
         // Get width of each column
         var result = [],
-            nColumn = columns.length,
-            body = table.querySelector('.yla-table__body');
+            header = table.querySelector('.yla-table__header'),
+            body = table.querySelector('.yla-table__body'),
+            nColumn = header.children.length - 1,
+            start = selectable ? 1 : 0;
 
-        for (var i = 0; i < nColumn; i++) {
+        for (var i = start; i < nColumn; i++) {
             var cell = body.children[i];
             if (cell) result.push(cell.offsetWidth);
         }
@@ -109,6 +123,10 @@ var YlaTable = function () {
         template: _template,
         props: {
             loading: Boolean,
+            selectable: {
+                type: Boolean,
+                default: false
+            },
             columns: {
                 type: Array,
                 required: true
@@ -123,35 +141,43 @@ var YlaTable = function () {
             return {
                 scrollWidth: 0,
                 columnsWidth: [],
+                selected: [],
                 sortColumn: '',
                 sortDirection: ''
             }
         },
         computed: {
             columnTemplate() {
-                return this.columns
-                    .map(col => _createColumnTemplate(col.width, col.minWidth))
-                    .join(' ');
+                var templates = this.columns
+                    .map(col => _createColumnTemplate(col.width, col.minWidth));
+
+                if (this.selectable) templates.unshift('60px');
+                return templates.join(' ');
             },
             headerColumnTemplate() {
                 var templates = this.columns
                     .map(col => _createColumnTemplate(col.width, col.minWidth));
 
+                if (this.selectable) templates.unshift('60px');
                 templates.push(this.scrollWidth + 'px');
                 return templates.join(' ');
             },
             finalColumns() {
                 return this.columns.map((col, idx) => {
-                    var textAlignment = col.align;
+                    var name = col.name || 'col-' + idx,
+                        label = col.label || '',
+                        sortName = col.sortName || name,
+                        textAlignment = col.align;
 
                     if (textAlignment !== 'center' && textAlignment !== 'right') {
                         textAlignment = undefined;
                     }
 
                     return {
-                        name: col.name || 'col-' + idx,
-                        label: col.label || '',
+                        name: name,
+                        label: label,
                         sortable: col.sortable === true,
+                        sortName: sortName,
                         width: col.width,
                         minWidth: col.minWidth,
                         align: textAlignment
@@ -165,7 +191,7 @@ var YlaTable = function () {
                 handler() {
                     this.$nextTick(() => {
                         this.scrollWidth = _getScrollWidth(this.$el);
-                        this.columnsWidth = _getColumnWidth(this.$el, this.columns, this.contents);
+                        this.columnsWidth = _getColumnWidth(this.$el, this.contents, this.selectable);
                         _resetScroll(this.$el);
                     });
                 }
@@ -174,12 +200,45 @@ var YlaTable = function () {
                 immediate: true,
                 handler() {
                     this.$nextTick(() => {
-                        this.columnsWidth = _getColumnWidth(this.$el, this.columns, this.contents);
+                        this.columnsWidth = _getColumnWidth(this.$el, this.contents, this.selectable);
                     });
                 }
             }
         },
         methods: {
+            headerSelectorIcon() {
+                if (this.selected.length === 0) return 'fa-square';
+                if (this.selected.length < this.contents.length) return 'fa-minus-square';
+                return 'fa-check-square';
+            },
+            selectorIcon(index) {
+                if (this.selected.indexOf(index) === -1) return 'fa-square';
+                return 'fa-check-square';
+            },
+            toggleAllSelection() {
+                var nSelected = this.selected.length,
+                    nContent = this.contents.length;
+
+                this.selected = [];
+                if (nSelected !== nContent) {
+                    for (var i = 0; i < nContent; i++) {
+                        this.selected.push(i);
+                    }
+                }
+
+                this.$emit('selected', this.selected);
+            },
+            toggleSelection(index) {
+                var pos = this.selected.indexOf(index);
+
+                if (pos !== -1) this.selected.splice(index, 1);
+                else {
+                    this.selected.push(index);
+                    this.selected.sort();
+                }
+
+                this.$emit('selected', this.selected);
+            },
             colWidth(index) {
                 var width = this.columnsWidth[index];
                 if (width != null) width += 'px';
@@ -202,7 +261,7 @@ var YlaTable = function () {
                 var columnName = column.sortName || column.name;
                 if (columnName !== this.sortColumn) {
                     this.sortColumn = columnName;
-                    this.sortDirection = 'asc';
+                    this.sortDirection = 'desc';
                 } else {
                     if (this.sortDirection === 'desc') {
                         this.sortDirection = 'asc';
@@ -211,6 +270,8 @@ var YlaTable = function () {
                     }
                 }
 
+                this.selected = [];
+                this.$emit('selected', this.selected);
                 this.$emit('sort', this.sortColumn + ' ' + this.sortDirection);
             }
         },
@@ -228,9 +289,15 @@ var YlaTable = function () {
 
                 window.addEventListener('resize', () => {
                     this.scrollWidth = _getScrollWidth(this.$el);
-                    this.columnsWidth = _getColumnWidth(this.$el, this.columns, this.contents);
+                    this.columnsWidth = _getColumnWidth(this.$el, this.contents, this.selectable);
                     _resetScroll(this.$el);
                 });
+            });
+        },
+        activated() {
+            this.$nextTick(() => {
+                this.scrollWidth = _getScrollWidth(this.$el);
+                this.columnsWidth = _getColumnWidth(this.$el, this.contents, this.selectable);
             });
         }
     }
